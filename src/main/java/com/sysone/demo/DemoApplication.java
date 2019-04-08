f90d4f7eaa4bb2b683f460ce87279ae93127769e
Claudio Caballero <claudiogomezcaballero@gmail.com>
2019-04-08 14:07:46 -0300

    correcciones

--- src/main/java/com/sysone/demo/DemoApplication.java
+++ src/main/java/com/sysone/demo/DemoApplication.java
@@ -2,11 +2,17 @@ package com.sysone.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan({"controller"})
 public class DemoApplication {
+
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DemoApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
