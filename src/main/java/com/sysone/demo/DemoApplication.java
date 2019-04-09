4a72b5fea63c2e2aabf0d372fcebcaa36ca96fce
Claudio Caballero <claudiogomezcaballero@gmail.com>
2019-04-09 00:38:17 -0300

    mejoras logger y refactoring

--- src/main/java/com/sysone/demo/DemoApplication.java
+++ src/main/java/com/sysone/demo/DemoApplication.java
@@ -1,5 +1,7 @@
 package com.sysone.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,17 +9,15 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"controller"})
+@ComponentScan({"controller", "service"})
 @EnableAutoConfiguration
 public class DemoApplication {
 
-
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(DemoApplication.class);
-	}
+	private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		logger.info("Aplicacion iniciada.");
 	}
 
 }
--- src/main/java/controller/SysOneController.java
+++ src/main/java/controller/SysOneController.java
@@ -1,14 +1,16 @@
 package controller;
 
+import service.SysOneService;
+import com.sysone.demo.DemoApplication;
 import controller.dto.candidate.CandidateOutDTO;
-import controller.dto.compress.CompressInDTO;
 import controller.dto.compress.CompressOutDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.ListIterator;
 
 /**
  * Created by C.CLAGOM on 4/5/2019.
@@ -16,63 +18,32 @@ import java.util.ListIterator;
 @RestController
 public class SysOneController {
 
+    @Autowired
+    private SysOneService sysOneService;
+
+    private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 
     @RequestMapping(value = "/compress",
             produces = {"application/json"},
             method = {RequestMethod.GET, RequestMethod.POST})
     @PostMapping(path="/compress")
     public ResponseEntity<CompressOutDTO> compressString(@RequestParam ("value") String value) {
+        logger.info("servicio /compress - inicio ejecucion");
         CompressOutDTO resultado = null;
-        CompressInDTO compressInDTO = new CompressInDTO();
-        compressInDTO.setValue(value);
-        if (!compressInDTO.getValue().isEmpty()) {
+        if (!value.isEmpty()) {
             resultado = new CompressOutDTO();
-            List<String> list = convertStringToList(compressInDTO.getValue());
-            StringBuffer stringbuffer = compress(list);
-            resultado.setValue(compressInDTO.getValue());
+            List<String> list = sysOneService.convertStringToList(value);
+            StringBuffer stringbuffer = sysOneService.compress(list);
+            resultado.setValue(value);
             resultado.setCompressed(stringbuffer.toString());
         }
-
         if (resultado == null) {
             return ResponseEntity.noContent().build();
         } else {
+            logger.info("servicio /compress - ejecucion exitosa");
+            logger.info("servicio /compress - resultado: " + resultado.toString());
             return ResponseEntity.ok(resultado);
         }
-    }
-
-
-    public StringBuffer compress(List<String> items) {
-        String prev = "";
-        String next = "";
-        StringBuffer strinbuffer= new StringBuffer();
-        long count = 1;
-        ListIterator<String> listIterator = items.listIterator();
-        if (listIterator.hasNext()) {
-            prev = listIterator.next();
-            while (listIterator.hasNext()){
-                next = listIterator.next();
-                if (next.equals(prev)) {
-                    count++;
-                } else {
-                    strinbuffer.append(count + prev);
-                    count = 1;
-                }
-                prev = next;
-                if (!listIterator.hasNext()) {
-                    strinbuffer.append(count + prev);
-                }
-            }
-        }
-        return strinbuffer;
-    }
-
-
-    public List<String> convertStringToList(String value) {
-        List<String> list = new ArrayList<>();
-        for (int i = 0; i < value.length(); i++) {
-            list.add(String.valueOf(value.charAt(i)).toUpperCase());
-        }
-        return list;
     }
 
 
@@ -83,6 +54,7 @@ public class SysOneController {
     public ResponseEntity<CandidateOutDTO> candidate() {
         CandidateOutDTO resultado = new CandidateOutDTO();
         resultado.setValue("Claudio Gomez Caballero");
+        logger.info("/candidate - ejecucion finalizada");
         return ResponseEntity.ok(resultado);
     }
 
